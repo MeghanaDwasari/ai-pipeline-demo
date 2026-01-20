6ea2ddcb067697af182b9428ecfdfa0be63debf3
meghana <meghanadwasari@gmail.com>
2026-01-20 14:27:40 +0530

    Added interest calculation and unit test

---
+++ test_interest.py
@@ -0,0 +1,7 @@
+from interest import calculate_interest
+
+def test_interest_calculation():
+    balance = 1000
+    rate = 0.05
+    expected_interest = 50
+    assert calculate_interest(balance, rate) == expected_interest
