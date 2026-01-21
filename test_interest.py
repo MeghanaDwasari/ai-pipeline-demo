52a6967b9b69047ab4ec761a77a73b7de6a99e15
MeghanaDwasari <meghanadwasari@gmail.com>
2026-01-21 13:39:33 +0530

    Update test_interest.py

--- test_interest.py
+++ test_interest.py
@@ -3,5 +3,5 @@ from interest import calculate_interest
 def test_interest_calculation():
     balance = 1000
     rate = 0.05
-    expected_interest = 50
-    assert calculate_interest(balance, rate) == expected_interest
+    expected = 50
+    assert calculate_interest(balance, rate) == expected
